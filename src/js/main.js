392c40104668b3e230f3884b874129503385654b
Oliver Findl <github@findl.sk>
2019-03-03 10:48:38 +0100

    Added dummy content

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@
 
 (function() {
 
-	/* YOUR CODE HERE */
+	const input = document.getElementById("input");
+
+	document.getElementById("button").addEventListener("click", event => {
+		event.preventDefault();
+
+		const message = `Hello ${input.value ? input.value : "world"}!`;
+		alert(message);
+		console.log(message); // eslint-disable-line no-console
+	});
 
 })();
