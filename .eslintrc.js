93d8f9518da965c7e9dfa7e6f22779c94eb56982
Oliver Findl <github@findl.sk>
2019-03-03 11:01:57 +0100

    Fixed mixed tabs and spaces in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 "use strict";
 
 module.exports = {
-    "env": {
+	"env": {
 		"browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
+		"es6": true
+	},
+	"extends": [
+		"eslint:recommended"
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module"
+	},
+	"rules": {
 		"indent": [
 			"error",
 			"tab"
